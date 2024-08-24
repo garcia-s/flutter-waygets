e36ea35a05bb3446be3c6d4e528b5005ea5ead58
Juan García <43949658+garcia-s@users.noreply.github.com>
2024-08-24 16:15:02 -0500

    getting the shm

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"github.com/neurlang/wayland/os"
 	"github.com/neurlang/wayland/wl"
 	"github.com/neurlang/wayland/wlclient"
 	"log"
@@ -9,11 +9,38 @@ import (
 
 func (s SurfaceHaver) HandleRegistryGlobalRemove(ev wl.RegistryGlobalRemoveEvent) {}
 func (s SurfaceHaver) HandleRegistryGlobal(ev wl.RegistryGlobalEvent) {
-	if ev.Interface == "wl_compositor" {
+
+	switch ev.Interface {
+	case "wl_compositor":
 		s.cptr = wlclient.RegistryBindCompositorInterface(s.reg, ev.Name, 3)
-        wl.Surface
+		s.cptr.CreateSurface()
+		var err error
+		s.surf, err = s.cptr.CreateSurface()
+		size := 300 * 4 * 300
+
+		if err != nil {
+			log.Fatalf("Failed to get the surface %v", err)
+		}
+
+		fd, err := os.CreateAnonymousFile(int64(3000))
+		_, err = os.Mmap(int(fd.Fd()), 0,
+			size, os.ProtRead|os.ProtWrite, os.MapShared)
+
+		if err != nil {
+			log.Fatalf("Failed to get the surface %v", err)
+		}
+
+		if err != nil {
+			log.Fatalf("failed to create an shm file %v", err)
+		}
+
+	case "wl_shm":
+		s.shm = wlclient.RegistryBindShmInterface(s.reg, ev.Name, 1)
+		// wlclient.ShmAddListener(s.shm, )
+
+	default:
+
 	}
-	fmt.Printf("Hello, ev %v\n", ev)
 }
 
 type SurfaceHaver struct {
@@ -21,6 +48,8 @@ type SurfaceHaver struct {
 	reg  *wl.Registry
 	disp *wl.Display
 	cptr *wl.Compositor
+	surf *wl.Surface
+	shm  *wl.Shm
 }
 
 func NewSurfaceHaver() (*SurfaceHaver, error) {
@@ -52,8 +81,8 @@ func NewSurfaceHaver() (*SurfaceHaver, error) {
 
 func main() { // Connect to the Wayland display
 
-	haver, err := NewSurfaceHaver()
-    surface = haver. 
+	_, err := NewSurfaceHaver()
+
 	if err != nil {
 		log.Fatalf("Error al intentar obener el compositor: %v", err)
 	}
