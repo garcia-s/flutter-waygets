a5f0413c8361ab1801156bd40ec95927f7728fdd
Juan García <43949658+garcia-s@users.noreply.github.com>
2024-10-12 16:32:04 -0500

    Adding the hic map

---
+++ src/keyboard/hic.zig
@@ -0,0 +1,106 @@
+pub const KEY_LEFT_CTRL = 0xE0;
+pub const KEY_LEFT_SHIFT = 0xE1;
+pub const KEY_LEFT_ALT = 0xE2;
+pub const KEY_LEFT_GUI = 0xE3; // Windows/Command key
+pub const KEY_RIGHT_CTRL = 0xE4;
+pub const KEY_RIGHT_SHIFT = 0xE5;
+pub const KEY_RIGHT_ALT = 0xE6;
+pub const KEY_RIGHT_GUI = 0xE7; // Windows/Command key
+
+// Alphanumeric keys
+pub const KEY_A = 0x04;
+pub const KEY_B = 0x05;
+pub const KEY_C = 0x06;
+pub const KEY_D = 0x07;
+pub const KEY_E = 0x08;
+pub const KEY_F = 0x09;
+pub const KEY_G = 0x0A;
+pub const KEY_H = 0x0B;
+pub const KEY_I = 0x0C;
+pub const KEY_J = 0x0D;
+pub const KEY_K = 0x0E;
+pub const KEY_L = 0x0F;
+pub const KEY_M = 0x10;
+pub const KEY_N = 0x11;
+pub const KEY_O = 0x12;
+pub const KEY_P = 0x13;
+pub const KEY_Q = 0x14;
+pub const KEY_R = 0x15;
+pub const KEY_S = 0x16;
+pub const KEY_T = 0x17;
+pub const KEY_U = 0x18;
+pub const KEY_V = 0x19;
+pub const KEY_W = 0x1A;
+pub const KEY_X = 0x1B;
+pub const KEY_Y = 0x1C;
+pub const KEY_Z = 0x1D;
+
+// Number keys
+pub const KEY_1 = 0x1E;
+pub const KEY_2 = 0x1F;
+pub const KEY_3 = 0x20;
+pub const KEY_4 = 0x21;
+pub const KEY_5 = 0x22;
+pub const KEY_6 = 0x23;
+pub const KEY_7 = 0x24;
+pub const KEY_8 = 0x25;
+pub const KEY_9 = 0x26;
+pub const KEY_0 = 0x27;
+
+// Function keys
+pub const KEY_F1 = 0x3A;
+pub const KEY_F2 = 0x3B;
+pub const KEY_F3 = 0x3C;
+pub const KEY_F4 = 0x3D;
+pub const KEY_F5 = 0x3E;
+pub const KEY_F6 = 0x3F;
+pub const KEY_F7 = 0x40;
+pub const KEY_F8 = 0x41;
+pub const KEY_F9 = 0x42;
+pub const KEY_F10 = 0x43;
+pub const KEY_F11 = 0x44;
+pub const KEY_F12 = 0x45;
+
+// Control keys
+pub const KEY_ENTER = 0x28;
+pub const KEY_ESC = 0x29;
+pub const KEY_BACKSPACE = 0x2A;
+pub const KEY_TAB = 0x2B;
+pub const KEY_SPACE = 0x2C;
+pub const KEY_DELETE = 0x4C;
+pub const KEY_CAPS_LOCK = 0x39;
+
+// Arrow keys
+pub const KEY_ARROW_RIGHT = 0x4F;
+pub const KEY_ARROW_LEFT = 0x50;
+pub const KEY_ARROW_DOWN = 0x51;
+pub const KEY_ARROW_UP = 0x52;
+
+// Miscellaneous keys
+pub const KEY_HOME = 0x4A;
+pub const KEY_PAGE_UP = 0x4B;
+pub const KEY_PAGE_DOWN = 0x4E;
+pub const KEY_END = 0x4D;
+pub const KEY_PRINT_SCREEN = 0x46;
+pub const KEY_SCROLL_LOCK = 0x47;
+pub const KEY_PAUSE = 0x48;
+pub const KEY_INSERT = 0x49;
+
+// Numpad keys
+pub const KEY_NUM_LOCK = 0x53;
+pub const KEY_NUMPAD_DIVIDE = 0x54;
+pub const KEY_NUMPAD_MULTIPLY = 0x55;
+pub const KEY_NUMPAD_SUBTRACT = 0x56;
+pub const KEY_NUMPAD_ADD = 0x57;
+pub const KEY_NUMPAD_ENTER = 0x58;
+pub const KEY_NUMPAD_1 = 0x59;
+pub const KEY_NUMPAD_2 = 0x5A;
+pub const KEY_NUMPAD_3 = 0x5B;
+pub const KEY_NUMPAD_4 = 0x5C;
+pub const KEY_NUMPAD_5 = 0x5D;
+pub const KEY_NUMPAD_6 = 0x5E;
+pub const KEY_NUMPAD_7 = 0x5F;
+pub const KEY_NUMPAD_8 = 0x60;
+pub const KEY_NUMPAD_9 = 0x61;
+pub const KEY_NUMPAD_0 = 0x62;
+pub const KEY_NUMPAD_DOT = 0x63;
