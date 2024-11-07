d619da5564140cd385a6114419af0fa1c8b9d314
Juan García <43949658+garcia-s@users.noreply.github.com>
2024-11-07 12:44:28 -0500

    We can now have composite characters in this thing

--- src/textinput/manager.zig
+++ src/textinput/manager.zig
@@ -25,7 +25,6 @@ pub const InputManager = struct {
 
     current_id: i64 = 0,
     current_key: ?u32 = null,
-    key_buff: []u8 = undefined,
     json_buff: []u8 = undefined,
     text_client: ?TextInputClient = null,
     editing_value: ?EditingValue = null,
@@ -37,10 +36,6 @@ pub const InputManager = struct {
 
     pub fn init(self: *InputManager, xkb: *XKBState) !void {
         self.xkb = xkb;
-        self.key_buff = self.gp.allocator().alloc(u8, 2) catch {
-            return;
-        };
-
         self.json_buff = self.gp.allocator().alloc(u8, 1024) catch {
             return;
         };
@@ -67,32 +62,53 @@ pub const InputManager = struct {
             => self.handle_char(engine, key),
 
             //TODO: MISSING COPY, PASTE AND OTHER GOODIES
-            else => {
-                return;
-            },
+            else => return,
         }
     }
 
-    fn handle_char(
-        self: *InputManager,
-        engine: c.FlutterEngine,
-        key: u32,
-    ) void {
+    // static void inputText(_GLFWwindow* window, uint32_t scancode)
+    // {
+    //     const xkb_keysym_t* keysyms;
+    //     const xkb_keycode_t keycode = scancode + 8;
+    //
+    //     if (xkb_state_key_get_syms(_glfw.wl.xkb.state, keycode, &keysyms) == 1)
+    //     {
+    //         const xkb_keysym_t keysym = composeSymbol(keysyms[0]);
+    //         const uint32_t codepoint = _glfwKeySym2Unicode(keysym);
+    //         if (codepoint != GLFW_INVALID_CODEPOINT)
+    //         {
+    //             const int mods = _glfw.wl.xkb.modifiers;
+    //             const int plain = !(mods & (GLFW_MOD_CONTROL | GLFW_MOD_ALT));
+    //             _glfwInputChar(window, codepoint, mods, plain);
+    //         }
+    //     }
+    // }
 
-        // var ksym: *c.xkb_keysym_t = null;
-        const end = c.xkb_state_key_get_utf8(
-            self.xkb.state,
-            key + 8,
-            self.key_buff.ptr,
-            self.key_buff.len,
-        );
+    fn handle_char(self: *InputManager, engine: c.FlutterEngine, scancode: u32) void {
+        var keysyms: [*c]c.xkb_keysym_t = undefined;
+        const keycode = scancode + 8;
+        // const sym = self.compose_symbol(key + 8);
+        if (c.xkb_state_key_get_syms(self.xkb.state, keycode, &keysyms) != 1)
+            return;
+
+        const keysym = self.compose_symbol(keysyms[0]);
+        //
+        //
+
+        if (keysym == c.XKB_KEY_NoSymbol) return;
+
+        var code_points: [4]u8 = undefined;
+        const end = std.unicode.utf8Encode(@intCast(keysym), &code_points) catch {
+            std.debug.print("Failed to encode unicode symbol", .{});
+            return;
+        };
 
         self.editing_value.?.text = std.fmt.allocPrint(
             self.gp.allocator(),
             "{s}{s}",
             .{
                 self.editing_value.?.text,
-                self.key_buff[0..@intCast(end)],
+                code_points[0..end],
             },
         ) catch return;
 
@@ -103,6 +119,25 @@ pub const InputManager = struct {
             @intCast(self.editing_value.?.text.len);
 
         self.dispatch_input_event(engine);
+    }
+
+    fn compose_symbol(self: *InputManager, sym: u32) u32 {
+        if (sym == c.XKB_KEY_NoSymbol or self.xkb.compose == null)
+            return sym;
+        if (c.xkb_compose_state_feed(self.xkb.compose, sym) != c.XKB_COMPOSE_FEED_ACCEPTED)
+            return sym;
+
+        switch (c.xkb_compose_state_get_status(self.xkb.compose)) {
+            c.XKB_COMPOSE_COMPOSED => {
+                return c.xkb_compose_state_get_one_sym(
+                    self.xkb.compose,
+                );
+            },
+            c.XKB_COMPOSE_COMPOSING, c.XKB_COMPOSE_CANCELLED => {
+                return c.XKB_KEY_NoSymbol;
+            },
+            else => return sym,
+        }
     }
 
     ///This function handles the keyboard whenever an input field is focused
@@ -137,12 +172,17 @@ pub const InputManager = struct {
     pub fn handle_backspace(self: *InputManager, engine: c.FlutterEngine) void {
         var edit = &(self.editing_value orelse return);
         const len = edit.text.len;
-        if (len != 0) {
-            edit.text = edit.text[0 .. len - 1];
-            self.dispatch_input_event(engine);
-            return;
+        if (len == 0) return;
+        var i = len;
+        while (i > 0) {
+            i -= 1;
+            if (edit.text[i] & 0xC0 != 0x80) {
+                edit.text = edit.text[0..i];
+                break;
+            }
         }
-        //TODO: maybe hook it up to a system sound efect
+        self.dispatch_input_event(engine);
+        return;
     }
 
     pub fn handle_submit(self: *InputManager, engine: c.FlutterEngine) void {
